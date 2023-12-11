20ec1e37bcaeb3f0a31a482a684c8665116d35f9
JosieEsi <josephine.kpodo@amalitech.org>
2023-12-11 23:58:00 +0000

    added more files, and updated the style.css

--- app.ts
+++ app.ts
@@ -1,1 +1,33 @@
-console.log('My code');
+console.log('My code......');
+
+// Assuming you're working in a TypeScript file (e.g., script.ts)
+
+// Define an interface to represent the structure of the API response
+interface Country {
+    name: string;
+    capital: string;
+    population: number;
+    // Add more properties as needed
+  }
+  
+  // Function to fetch data from the API
+  async function fetchData(): Promise<void> {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const data: Country[] = await response.json();
+      
+      // Now you can use the 'data' variable which contains the parsed JSON response
+      console.log(data);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    }
+  }
+  
+  // Call the fetchData function
+  fetchData();
+  
